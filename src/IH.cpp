7322dcf5b1e80056b6c912fb0cdfea30ba473d86
RileyA <praetor57@gmail.com>
2011-03-06 22:46:57 -0800

    Fixed some more indenting.

--- src/IH.cpp
+++ src/IH.cpp
@@ -7,23 +7,23 @@ int main(int argc, char** argv)
 	Logger::getPtr();
 	TimeManager::getPtr();
 
-    // create subsystems:
+	// create subsystems:
 	ChaiscriptSubsystem chaiSys = ChaiscriptSubsystem();
 	ALSubsystem aSys = ALSubsystem();
 	OgreSubsystem ogre = OgreSubsystem(1024,768,false);
 	OISSubsystem ois = OISSubsystem();
 	BulletSubsystem bull = BulletSubsystem();
 
-    // allocate engine and add subsystems
+	// allocate engine and add subsystems
 	Engine* eng = new Engine();
 	eng->addSubsystem(&chaiSys);
 	eng->addSubsystem(&aSys);
 	eng->addSubsystem(&bull);
-    eng->addSubsystem(&ogre);
+	eng->addSubsystem(&ogre);
 	eng->addSubsystem(&ois);
 
-    // initialize the engine
-    eng->init();
+	// initialize the engine
+	eng->init();
 
 	// add the testing state to the stack
 	eng->addState(new IH::MenuState());
@@ -31,8 +31,8 @@ int main(int argc, char** argv)
 	// start up the engine
 	eng->start();
 
-    // delete the engine object
-    delete eng;
+	// delete the engine object
+	delete eng;
 
 	return 0;
 }
